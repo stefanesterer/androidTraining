28e14962a5cf9b2bbac0a726ac8a7d2e0c55beef
Stefan Esterer <stefan.esterer@gmail.com>
2017-05-21 18:58:27 +0200

    add "up button" to child activity

--- app/src/main/java/com/example/stefan/myapplication/DisplayMessageActivity.java
+++ app/src/main/java/com/example/stefan/myapplication/DisplayMessageActivity.java
@@ -3,7 +3,10 @@ package com.example.stefan.myapplication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
 
@@ -28,17 +31,31 @@ public class DisplayMessageActivity extends AppCompatActivity {
             }
         }
 
+        Toolbar myChildToolbar = (Toolbar) findViewById(R.id.activity_display_message_toolbar);
+        setSupportActionBar(myChildToolbar);
+
+        // Get a support ActionBar corresponding to this toolbar
+        ActionBar ab = getSupportActionBar();
+
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
+
     }
 
-    public void switchFragment(View view)
-    {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    public void switchFragment(View view) {
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.fragment_layout, new ArticleFragment());
         transaction.addToBackStack(null);
         transaction.commit();
 
     }
-
 
 
 }
